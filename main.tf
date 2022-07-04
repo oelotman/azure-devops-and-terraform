cdac5d2d5bb429ae9e9e8668f6c8ce9fcf3661b4
otmane.elotmani <el.otmani.otmane@gmail.com>
2022-07-04 21:14:47 +0100

    add main

---
+++ main.tf
@@ -0,0 +1,51 @@
+# provider "azurerm" {
+#   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+#   version = "=2.5.0"
+#   features {}
+# }
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.92.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+    use_msi = true
+  # Configuration options
+}
+resource "azurerm_resource_group" "tf_test" {
+  name     = "rg_test"
+  location = "West Europe"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+  name                = "weatherapi01"
+  location            = azurerm_resource_group.tf_test.location
+  resource_group_name = azurerm_resource_group.tf_test.name
+  ip_address_type     = "public"
+  dns_name_label      = "weatherapi01oeo"
+  os_type             = "Linux"
+
+  container {
+    name   = "ct001"
+    image  = "otmane1990/weatherapi"
+    cpu    = "1"
+    memory = "1"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
+  tags = {
+    environment = "testing"
+  }
+}
+output "aaaaa" {
+  value = azurerm_resource_group.tf_test.location #  type de ressource . Nom ressource terraform . paramettre rechrché 
+}
